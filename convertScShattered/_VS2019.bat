690a1e6259c40bcc0ab7e3cdec6b8821f57f8459
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2024-02-19 13:50:12 +0900

    Ported v2024

--- convertScShattered/_VS2019.bat
+++ convertScShattered/_VS2019.bat
@@ -1,4 +1,4 @@
-SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2023_SP2_U1
-SET HCOMMUNICATOR_INSTALL_DIR=C:\SDK\Communicator\HOOPS_Communicator_2023_SP2_U1
+SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2024
+SET HCOMMUNICATOR_INSTALL_DIR=C:\SDK\Communicator\HOOPS_Communicator_2024
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" convertScShuttered.sln
